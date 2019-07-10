78952c29a42566d3555fbd6967503a51b1fde1e8
Márcio Koch <lobokoch@gmail.com>
2019-07-10 20:28:04 -0300

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+echo "Starting build generation..."
 cd modules
+
+echo "Packing modules..."
 mvn clean package
+
 cd app
+
+echo "Building Docker image..."
 mvn dockerfile:build
+
+echo "DONE"
